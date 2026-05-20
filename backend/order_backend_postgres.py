30436c953ff37072329bd29999367d0806176b05
Niraj242 <nirajhkurane@gmail.com>
2026-05-20 22:51:08 +0530

    Fix cloud database configuration

--- backend/order_backend_postgres.py
+++ backend/order_backend_postgres.py
@@ -19,10 +19,11 @@ from sqlalchemy.orm import sessionmaker, Session
 import requests
 import datetime
 import uuid
+import os
 
 # ================= DATABASE =================
-DATABASE_URL = "postgresql://postgres:Niraj%40242@localhost:5432/scm_db"
-engine = create_engine(DATABASE_URL)
+DATABASE_URL = os.getenv("postgresql://neondb_owner:npg_EeyF0hi3JRcV@ep-wild-resonance-aoturwvf.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require")
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
 
