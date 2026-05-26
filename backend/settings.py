493f8016da071e097e1bc5ceeb5261da0f4a0f22
Niraj242 <nirajhkurane@gmail.com>
2026-05-26 15:44:59 +0530

    Create settings.py

---
+++ backend/settings.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    JOBWORK_REPORT_URL: str
+    TRB_MASTER_URL: str
+    DGBB_MASTER_URL: str
+    TRACEABILITY_MASTER_URL: str
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
